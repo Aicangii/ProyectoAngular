bf925fa36cec216b93d108e6fe9feeb2c8fe5666
Aicangii <44418890+Aicangii@users.noreply.github.com>
2019-11-13 10:24:00 -0300

    arreglo
    la pura caga

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './Proyectoangular/header/header.component';
+import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { MainComponent } from './main/main.component';
 import { MenuComponent } from './menu/menu.component';
